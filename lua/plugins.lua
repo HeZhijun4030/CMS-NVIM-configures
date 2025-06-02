47ec2f6267efe4f7b62d5c4b9e5683fa6e6333a4
HeZhijun4030 <Zhijun4030@outlook.com>
2025-06-02 12:29:41 +0800

    init1

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -25,7 +25,16 @@ return require('packer').startup(function(use)
         require'alpha'.setup(require'alpha.themes.dashboard'.config)
     end
   } 
-  use 'nvim-neo-tree/neo-tree.nvim'
+  use({
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    requires = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    }
+  })
 
   use 'nvim-treesitter/nvim-treesitter'
   use 'lukas-reineke/indent-blankline.nvim'
